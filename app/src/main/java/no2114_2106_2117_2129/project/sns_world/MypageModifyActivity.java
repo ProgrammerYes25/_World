25efdc03aa9be0b57dabf4a0616863d2e73c4699
yday05506 <yangdayeon2@gmail.com>
2023-02-09 17:06:56 +0900

    My프로필 레이아웃(미완성)

--- app/src/main/java/no2114_2106_2117_2129/project/sns_world/MypageModifyActivity.java
+++ app/src/main/java/no2114_2106_2117_2129/project/sns_world/MypageModifyActivity.java
@@ -9,6 +9,6 @@ public class MypageModifyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mypagemodify);
+        setContentView(R.layout.activity_my_page_modify);
     }
 }
