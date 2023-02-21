1801899b532a06bbe166bbf041239a3dc0c3061b
yday05506 <yangdayeon2@gmail.com>
2023-02-21 14:42:00 +0900

    MY 알림, MY페이지 내가 쓴 글 전체보기

---
+++ app/src/main/java/no2114_2106_2117_2129/project/sns_world/MyNoticeActivity.java
@@ -0,0 +1,14 @@
+package no2114_2106_2117_2129.project.sns_world;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyNoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_notice);
+    }
+}
