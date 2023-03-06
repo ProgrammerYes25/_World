74ae1eca88ba9336eddb20cd9fbda0a2ac229af8
yday05506 <yangdayeon2@gmail.com>
2023-03-07 00:01:57 +0900

    my 프로필 약간 수정

--- app/src/main/java/no2114_2106_2117_2129/project/sns_world/MypageModifyFragment.java
+++ app/src/main/java/no2114_2106_2117_2129/project/sns_world/MypageModifyFragment.java
@@ -15,7 +15,7 @@ public class MypageModifyFragment extends Fragment {
     EditText nameEditText;  // Y - 이름 edit text
     EditText ageEditText;   // Y - 나이 edit text
     EditText interestEditText;  // Y - 관심사 edit text
-
+    EditText myProfileEditText; // Y - 자기소개 한 마디 edit text
 
     // Y - 성별 spinner 선언
     Spinner genderSpinner;
